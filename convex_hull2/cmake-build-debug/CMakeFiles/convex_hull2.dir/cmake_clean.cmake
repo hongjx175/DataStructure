c5b4e16f37c3e2677fb6e4db25832f704ce314fc
H-primes <1751215626@qq.com>
2020-09-29 19:54:28 +0800

    convex hull 2

---
+++ convex_hull2/cmake-build-debug/CMakeFiles/convex_hull2.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/convex_hull2.dir/main.cpp.obj"
+  "convex_hull2.exe"
+  "convex_hull2.exe.manifest"
+  "convex_hull2.pdb"
+  "libconvex_hull2.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/convex_hull2.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()

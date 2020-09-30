2ac14f3e6011e27d454f913134f26d67d799292e
H-primes <1751215626@qq.com>
2020-09-30 10:06:06 +0800

    deal cmake

---
+++ quicksort/cmake-build-debug/CMakeFiles/quicksort.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/quicksort.dir/main.cpp.obj"
+  "libquicksort.dll.a"
+  "quicksort.exe"
+  "quicksort.exe.manifest"
+  "quicksort.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/quicksort.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()

7139389ed3e76b96a7e1a7a21e8977b93bff44a3
H-primes <1751215626@qq.com>
2020-10-12 15:37:10 +0800

    create

---
+++ BinaryTree/cmake-build-debug/CMakeFiles/BinaryTree.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "BinaryTree.exe"
+  "BinaryTree.exe.manifest"
+  "BinaryTree.pdb"
+  "CMakeFiles/BinaryTree.dir/main.cpp.obj"
+  "libBinaryTree.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/BinaryTree.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()

2748e5d86943d70ca1417611966a1af0c7547647
H-primes <1751215626@qq.com>
2020-10-21 09:40:44 +0800

    create bst

---
+++ BST/BST.h
@@ -0,0 +1,8 @@
+//
+// Created by 86185 on 2020/10/21.
+//
+
+#ifndef BST_BST_H
+#define BST_BST_H
+
+#endif//BST_BST_H
--- BinaryTree/main.cpp
+++ BinaryTree/main.cpp
@@ -1,33 +1,33 @@
 #include "BinaryTree.h"
 #include <iostream>
 #include <vector>
-using std::vector;
+using std::cin;
 using std::cout;
 using std::endl;
-using std::cin;
+using std::vector;
 int main() {
-	BinaryTree<int>* binaryTree = new BinaryTree<int>();
+	BinaryTree<int> *binaryTree = new BinaryTree<int>();
 	//BinaryTree<T>::insertNode(T data, int Id, int father, int lChild, int rChild)
-	binaryTree->insertNode(0,0,-1,1,2);
-	binaryTree->insertNode(1,1,0,5,6);
-	binaryTree->insertNode(2,2,0,4,3);
-	binaryTree->insertNode(3,3,2,-1,-1);
-	binaryTree->insertNode(4,4,2,-1,8);
-	binaryTree->insertNode(5,5,1,-1,-1);
-	binaryTree->insertNode(6,6,1,7,-1);
-	binaryTree->insertNode(7,7,6,-1,-1);
-	binaryTree->insertNode(8,8,4,-1,-1);
+	binaryTree->insertNode(0, 0, -1, 1, 2);
+	binaryTree->insertNode(1, 1, 0, 5, 6);
+	binaryTree->insertNode(2, 2, 0, 4, 3);
+	binaryTree->insertNode(3, 3, 2, -1, -1);
+	binaryTree->insertNode(4, 4, 2, -1, 8);
+	binaryTree->insertNode(5, 5, 1, -1, -1);
+	binaryTree->insertNode(6, 6, 1, 7, -1);
+	binaryTree->insertNode(7, 7, 6, -1, -1);
+	binaryTree->insertNode(8, 8, 4, -1, -1);
 
-	cout<<"layerTraversal: ";
+	cout << "layerTraversal: ";
 	binaryTree->layerTraversal();//层次遍历
-	cout<<endl;
-	cout<<"preorderTraversal: ";
+	cout << endl;
+	cout << "preorderTraversal: ";
 	binaryTree->preorderTraversal();//前序遍历
-	cout<<endl;
-	cout<<"postorderTraversal: ";
+	cout << endl;
+	cout << "postorderTraversal: ";
 	binaryTree->postorderTraversal();//后序遍历
-	cout<<endl;
-	cout<<"midorderTraversal: ";
+	cout << endl;
+	cout << "midorderTraversal: ";
 	binaryTree->midorderTraversal();//中序遍历
 
 
@@ -36,36 +36,38 @@ int main() {
 	int preorder[] = {0, 1, 5, 6, 7, 2, 4, 8, 3};
 
 	delete binaryTree;
-	cout<<endl<<"----------reconstruct from postoder and midorder----------"<<endl;
+	cout << endl
+	     << "----------reconstruct from postoder and midorder----------" << endl;
 	binaryTree = new BinaryTree<int>();
 	binaryTree->restorePost(postorder, midorder, 9);
-	cout<<"layerTraversal: ";
+	cout << "layerTraversal: ";
 	binaryTree->layerTraversal();//层次遍历
-	cout<<endl;
-	cout<<"preorderTraversal: ";
+	cout << endl;
+	cout << "preorderTraversal: ";
 	binaryTree->preorderTraversal();//前序遍历
-	cout<<endl;
-	cout<<"postorderTraversal: ";
+	cout << endl;
+	cout << "postorderTraversal: ";
 	binaryTree->postorderTraversal();//后序遍历
-	cout<<endl;
-	cout<<"midorderTraversal: ";
+	cout << endl;
+	cout << "midorderTraversal: ";
 	binaryTree->midorderTraversal();//中序遍历
 
 
 	delete binaryTree;
-	cout<<endl<<"----------reconstruct from preoder and midorder----------"<<endl;
+	cout << endl
+	     << "----------reconstruct from preoder and midorder----------" << endl;
 	binaryTree = new BinaryTree<int>();
 	binaryTree->restorePre(preorder, midorder, 9);
-	cout<<"layerTraversal: ";
+	cout << "layerTraversal: ";
 	binaryTree->layerTraversal();//层次遍历
-	cout<<endl;
-	cout<<"preorderTraversal: ";
+	cout << endl;
+	cout << "preorderTraversal: ";
 	binaryTree->preorderTraversal();//前序遍历
-	cout<<endl;
-	cout<<"postorderTraversal: ";
+	cout << endl;
+	cout << "postorderTraversal: ";
 	binaryTree->postorderTraversal();//后序遍历
-	cout<<endl;
-	cout<<"midorderTraversal: ";
+	cout << endl;
+	cout << "midorderTraversal: ";
 	binaryTree->midorderTraversal();//中序遍历
 
 	return 0;

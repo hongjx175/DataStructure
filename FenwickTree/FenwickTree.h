a6748dbd0c3675fb1785220c28df6839bff01c89
H-primes <1751215626@qq.com>
2020-11-02 17:49:31 +0800

    done

--- FenwickTree/FenwickTree.h
+++ FenwickTree/FenwickTree.h
@@ -1,18 +1,46 @@
-//
-// Created by 86185 on 2020/11/2.
-//
-
 #ifndef FENWICKTREE_FENWICKTREE_H
 #define FENWICKTREE_FENWICKTREE_H
 #include <iostream>
 #include <vector>
 using std::vector;
-template<typename T>
-class FenwickTree{
+class FenwickTree {
 private:
-	vector<T> tree;
+	vector<int> *tree;
+	int lowbit(int x) { return x & (-x); }
+	int _query(int r);
 
 public:
-
+	FenwickTree(int *a, int n);
+	~FenwickTree();
+	int query(int l, int r);
+	void update(int x, int data);
 };
+
+FenwickTree::FenwickTree(int a[], int n) {
+	tree = new vector<int>(n, 0);
+	for (int i = 0; i < n; i++) {
+		for (int j = i + 1; j <= n; j += lowbit(j)) {
+			(*tree)[j - 1] += a[i];
+		}
+	}
+}
+
+int FenwickTree::query(int l, int r) {
+	return _query(r) - _query(l - 1);
+}
+
+int FenwickTree::_query(int r) {
+	if (r == -1) return 0;
+	int ans = 0;
+	for (int i = r + 1; i > 0; i -= lowbit(i)) {
+		ans += (*tree)[i - 1];
+	}
+	return ans;
+}
+void FenwickTree::update(int x, int data) {
+	int n = tree->size();
+	for (int i = x + 1; i <= n; i += lowbit(i)) {
+		(*tree)[i - 1] += data;
+	}
+}
 #endif//FENWICKTREE_FENWICKTREE_H
--- FenwickTree/main.cpp
+++ FenwickTree/main.cpp
@@ -1,9 +1,22 @@
 #include "FenwickTree.h"
+#include <ctime>
 #include <iostream>
-using std::cout;
 using std::cin;
+using std::cout;
 using std::endl;
+#define random() (rand() % 11)
 int main() {
-	std::cout << "Hello, World!" << std::endl;
+	unsigned seed = time(0);
+	srand(seed);
+	int a[300];
+	cout << "a[]: ";
+	for (int i = 0; i < 20; i++) {
+		a[i] = random();
+		cout << a[i] << "  ";
+	}
+	FenwickTree *fenwickTree = new FenwickTree(a, 20);
+	cout << "\nquery(0, 5): " << fenwickTree->query(0, 5) << endl;
+	fenwickTree->update(3, 1);
+	cout << "after a[3] adding 1, query(0, 5): " << fenwickTree->query(0, 5) << endl;
 	return 0;
 }

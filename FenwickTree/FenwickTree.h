35b8678b20566533370fe8ad71fc24d217cadf0d
H-primes <1751215626@qq.com>
2020-11-02 14:13:45 +0800

    create project fenwicktree

---
+++ FenwickTree/FenwickTree.h
@@ -0,0 +1,18 @@
+//
+// Created by 86185 on 2020/11/2.
+//
+
+#ifndef FENWICKTREE_FENWICKTREE_H
+#define FENWICKTREE_FENWICKTREE_H
+#include <iostream>
+#include <vector>
+using std::vector;
+template<typename T>
+class FenwickTree{
+private:
+	vector<T> tree;
+
+public:
+
+};
+#endif//FENWICKTREE_FENWICKTREE_H
---
+++ FenwickTree/main.cpp
@@ -0,0 +1,9 @@
+#include "FenwickTree.h"
+#include <iostream>
+using std::cout;
+using std::cin;
+using std::endl;
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}

f8f42058673980d505737dc58a118cdc135f7962
H-primes <1751215626@qq.com>
2020-11-16 19:07:51 +0800

    create

---
+++ 2-3 Tree/2_3Tree.h
@@ -0,0 +1,14 @@
+//
+// Created by 86185 on 2020/11/16.
+//
+
+#ifndef INC_2_3_TREE_2_3TREE_H
+#define INC_2_3_TREE_2_3TREE_H
+template<typename T>
+class Tree2_3{
+private:
+	struct Node{
+
+	};
+};
+#endif//INC_2_3_TREE_2_3TREE_H
---
+++ 2-3 Tree/main.cpp
@@ -0,0 +1,6 @@
+#include <iostream>
+#include "2_3Tree.h"
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
